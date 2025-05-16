bb5947d2c9dad2a94aff20e51e82e46f5c6a7993
Marcelo Marcon <marcelo.marcon@ax4b.com>
2025-05-16 15:33:50 -0400

    change: modificado versionamento do setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odoo-logtracer",
-    version="0.1.0",
+    version="0.2.1",
     description="Ferramentas de logging personalizadas",
     author="Marcelo Marcon",
     packages=find_packages(),
