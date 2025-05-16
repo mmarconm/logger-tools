8c8ca29d28036911325fd600adee97c1153b35c2
Marcelo Marcon <marcelo.marcon@ax4b.com>
2025-05-15 20:46:22 -0400

    bugfix: adicionado setup.py para evitar erro na instalação via pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logger-tools",
+    version="0.1.0",
+    description="Ferramentas de logging personalizadas",
+    author="Marcelo Marcon",
+    packages=find_packages(),
+    install_requires=[],
+)
